751e75d960c38d2c70917faba80a5d5980bf09ae
Akbar K <112651216+swiftdiary@users.noreply.github.com>
2025-01-24 16:15:02 +0500

    Initial Commit

---
+++ Alums/AlumsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlumsApp.swift
+//  Alums
+//
+//  Created by Akbar Khusanbaev on 24/01/25.
+//
+
+import SwiftUI
+
+@main
+struct AlumsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
